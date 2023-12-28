5e93395cd215995c96425490fa561341621dad6e
Matt Litzinger <matt@litzdigital.com>
2023-12-28 14:32:28 -0500

    Initial commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Branches",
+	url: "https://branchesgathering.com/",
+	language: "en",
+	description: "A local gathering of Christian creatives.",
+	author: {
+		name: "Matt Litzinger",
+		email: "matt@litzdigital.com",
+		url: "https://litzdigital.com/"
+	}
+}
---
+++ content/contact/index.md
@@ -0,0 +1,9 @@
+---
+layout: layouts/base.njk
+eleventyNavigation:
+  key: Contact
+  order: 3
+---
+# Contact Us
+
+Have questions about Branches Conference? Send us a message – we’re happy to help.
