1589f670bbc353ca579a8868f13afb5c18b47db7
Matt Litzinger <matt@litzdigital.com>
2025-01-06 15:35:39 -0500

    Removes Jess Claiter from Presenters page

--- _data/metadata.js
+++ _data/metadata.js
@@ -7,5 +7,6 @@ module.exports = {
 		name: "Matt Litzinger",
 		email: "matt@litzdigital.com",
 		url: "https://litzdigital.com/"
-	}
+	},
+	registration_url: "https://www.ticketleap.events/tickets/branches/2025-gathering"
 }
